1347091498913485da17f148046dd0d8a3d3d053
Matheus <132359958+XxXMatheusXxX@users.noreply.github.com>
2023-11-14 09:29:15 -0300

    Update PedidosController.java

--- GerenciamentoPedidos/src/main/java/com/yay/controller/PedidosController.java
+++ GerenciamentoPedidos/src/main/java/com/yay/controller/PedidosController.java
@@ -10,17 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yay.entities.Pedidos;
 import com.yay.services.PedidosService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
+@Tag(name = "Pedidos", description = "API REST DE GERENCIAMENTO DO Pedidos")
 @RestController
-@RequestMapping("/Pedidos")
+@RequestMapping("/pedidos")
+
 public class PedidosController {
 
 	private final PedidosService PedidosService;
@@ -31,6 +35,7 @@ public class PedidosController {
 	}
 
 	@GetMapping("/{id}")
+	@Operation(summary = "Localiza Pedidos por ID")
 	public ResponseEntity<Pedidos> buscaPedidosControlId(@PathVariable Long id) {
 		Pedidos Pedidos = PedidosService.getPedidosById(id);
 		if (Pedidos != null) {
@@ -40,19 +45,22 @@ public class PedidosController {
 		}
 	}
 
-	@GetMapping("/")
+	@GetMapping
+	@Operation(summary = "apresenta todos os Pedidoss")
 	public ResponseEntity<List<Pedidos>> buscaTodasLigacoesControl() {
 		List<Pedidos> Pedidos = PedidosService.getAllPedidoss();
 		return ResponseEntity.ok(Pedidos);
 	}
 
-	@PostMapping("/")
+	@PostMapping
+	@Operation(summary = "cadastra os Pedidoss")
 	public ResponseEntity<Pedidos> savePedidosControl(@RequestBody @Valid Pedidos Pedidos) {
 		Pedidos savePedidos = PedidosService.savePedidos(Pedidos);
 		return ResponseEntity.status(HttpStatus.CREATED).body(savePedidos);
 	}
 
 	@PutMapping("/{id}")
+	@Operation(summary = "altera os Pedidoss")
 	public ResponseEntity<Pedidos> alteraPedidosControl(@PathVariable Long id, @RequestBody @Valid Pedidos Pedidos) {
 		Pedidos alteraPedidos = PedidosService.changePedidos(id, Pedidos);
 
@@ -64,10 +72,11 @@ public class PedidosController {
 	}
 
 	@DeleteMapping("/{id}")
+	@Operation(summary = "deleta os Pedidoss")
 	public ResponseEntity<String> deletePedidosControl(@PathVariable Long id) {
 		boolean delete = PedidosService.deletePedidos(id);
 		if (delete) {
-			return ResponseEntity.ok().body("O Pedidos foi excluido com o sucesso");
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 		} else {
 			return ResponseEntity.notFound().build();
 		}
